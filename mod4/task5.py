6b687b0784e2bda946f62da102786647ecbd63bd
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-03-21 18:02:31 +0500

    Update task5.py

--- mod4/task5.py
+++ mod4/task5.py
@@ -11,8 +11,7 @@ def get_ps():
     args = request.args.getlist('arg')
     clean_user_cmd = [shlex.quote(arg) for arg in args]
     cmd = shlex.split(f'ps {" ".join(clean_user_cmd)}')
-    result = subprocess.run(cmd, stdout=subprocess.PIPE)
-    result = result.stdout.decode()
+    result = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode()
     return string.Template('<pre>${output}</pre>').substitute(output=result)
 
 
