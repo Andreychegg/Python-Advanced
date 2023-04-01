a2fe4842d3e2e03f8b9a72b028c7e419c1f679f3
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-04-01 17:36:47 +0500

    Create task3.py

---
+++ mod6/task3.py
@@ -0,0 +1,70 @@
+import json
+import re
+import getpass
+import hashlib
+import logging
+
+
+class JsonAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        msg = json.dumps(msg, ensure_ascii=False)
+        return msg, kwargs
+
+
+logger = JsonAdapter(logging.getLogger('password_checker'))
+
+
+def input_and_check_password():
+    logger.debug('Начало input_and_check_password')
+    password = getpass.getpass()
+
+    if not password:
+        logger.warning('Вы ввели пустой пароль.')
+        return False
+
+    if not is_strong_password(password):
+        logger.warning('Пароль содержит слова английского языка.')
+        return False
+
+    try:
+        hasher = hashlib.md5()
+
+        hasher.update(password.encode('latin-1'))
+
+        if hasher.hexdigest() == '098f6bcd4621d373cade4e832627b4f6':
+            return True
+
+    except ValueError as ex:
+        logger.exception('Вы ввели некорректный символ ', exc_info=False)
+
+    return False
+
+
+def is_strong_password(password):
+    with open('words.txt', 'r') as file:
+        words = file.read()
+    words = re.findall(r'\b\S{5,}\b', words)
+
+    for word in words:
+        if word.lower() in password.lower():
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='skillbox_json_messages.log',
+        level=logging.INFO,
+        format='{"time": "%(asctime)s", "level": "%(levelname)s", "message": %(message)s}', datefmt='%H:%M:%S'
+    )
+
+    count_number: int = 3
+    logger.info(f'Вы пытаетесь аутентифицироваться в Skillbox. У вас есть {count_number} попытки.')
+
+    while count_number > 0:
+        if input_and_check_password():
+            exit(0)
+        count_number -= 1
+
+    logger.error('Пользователь трижды ввёл неправильный пароль!')
+    exit(1)
