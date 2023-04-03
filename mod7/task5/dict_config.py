cd96b67474a45565bdb4ae55725b74b2824775f7
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-04-03 23:46:59 +0500

    Create dict_config.py

---
+++ mod7/task5/dict_config.py
@@ -0,0 +1,46 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from logs_levels import LogsLevels
+
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'level': 'INFO'
+        },
+        'file': {
+            '()': LogsLevels,
+            'formatter': 'standard'
+        },
+        'utils': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'standard',
+            'filename': 'utils.log',
+            'when': 'H',
+            'interval': 1,
+            'backupCount': 10
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'utils': {
+            'handlers': ['utils'],
+            'level': 'INFO',
+            'propagate': False
+        }
+    }
+}
