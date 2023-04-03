e0e68b97a33645e613d7aa3ce6f7e167624f6ff9
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-04-03 23:46:36 +0500

    Create utils.py

---
+++ mod7/task5/utils.py
@@ -0,0 +1,38 @@
+import logging.config
+from dict_config import dict_config
+
+logging.config.dictConfig(dict_config)
+logger = logging.getLogger(__name__)
+
+
+def add(x, y):
+    result = x + y
+    logging.debug(f"{x} + {y} = {result}")
+    logger.info(f"{x} + {y} = {result}")
+    return result
+
+
+def subtract(x, y):
+    result = x - y
+    logging.debug(f"{x} - {y} = {result}")
+    logger.info(f"{x} - {y} = {result}")
+    return result
+
+
+def multiply(x, y):
+    result = x * y
+    logging.debug(f"{x} * {y} = {result}")
+    logger.info(f"{x} * {y} = {result}")
+    return result
+
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        logging.error("На ноль делить нельзя")
+        logger.error("На ноль делить нельзя")
+        return None
+    logging.debug(f"{x} / {y} = {result}")
+    logger.info(f"{x} / {y} = {result}")
+    return result
