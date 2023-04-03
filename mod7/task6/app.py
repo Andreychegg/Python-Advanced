eb663139b34736f12438dca4714372a720e980f8
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-04-04 00:09:24 +0500

    Create app.py

---
+++ mod7/task6/app.py
@@ -0,0 +1,56 @@
+import logging.config
+import sys
+import utils
+from dict_config import dict_config
+import logging_tree
+
+
+def get_number():
+    while True:
+        try:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            return num1, num2
+        except ValueError:
+            logging.warning("Неверный ввод")
+
+
+def get_operation():
+    while True:
+        operation = input("+ - сложение \n- - вычитание \n* - умножение \n/ - деление \n0 - выход \nВыберите действие: ")
+        if operation in ["+", "-", "*", "/", "0"]:
+            return operation
+        logging.warning("Неверная операция: %s", operation)
+
+
+def main():
+    logging.info("Калькулятор запущен")
+    while True:
+        action = get_operation()
+
+        if action == "0":
+            logger.info("Калькулятор остановлен")
+            sys.exit()
+
+        nums = get_number()
+        x = nums[0]
+        y = nums[1]
+
+        if action == "+":
+            result = utils.add(x, y)
+        elif action == "-":
+            result = utils.subtract(x, y)
+        elif action == "*":
+            result = utils.multiply(x, y)
+        elif action == "/":
+            result = utils.divide(x, y)
+
+        logging.info(f"Результат: {result}")
+
+
+if __name__ == '__main__':
+    logging.config.dictConfig(dict_config)
+    logger = logging.getLogger(__name__)
+    with open('logging_tree.txt', 'w') as book:
+        book.write(logging_tree.format.build_description())
+    main()
