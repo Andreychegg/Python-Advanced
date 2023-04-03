23c4e2cd6acfd1ffba5319c6e1131670ca2f8ee7
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-04-04 02:26:15 +0500

    Update filter_ASCII.py

--- mod7/task8/filter_ASCII.py
+++ mod7/task8/filter_ASCII.py
@@ -3,5 +3,5 @@ import string
 
 
 class FilterASCII(logging.Filter):
-    def filter(self, record: logging.LogRecord) -> int:
+    def filter(self, record: logging.LogRecord):
         return not any(symbol not in string.printable for symbol in record.msg)
