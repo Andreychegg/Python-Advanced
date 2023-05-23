e7fb15a8ea0a88f6363bc0b6a0d7cac23cb99111
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-05-24 00:52:50 +0500

    Create task2.py

---
+++ mod12/task2.py
@@ -0,0 +1,33 @@
+import sys
+from threading import Semaphore, Thread
+import time
+
+sem: Semaphore = Semaphore()
+
+
+def fun1():
+    while True:
+        sem.acquire()
+        print(1)
+        sem.release()
+        time.sleep(0.25)
+
+
+def fun2():
+    while True:
+        sem.acquire()
+        print(2)
+        sem.release()
+        time.sleep(0.25)
+
+
+t1: Thread = Thread(target=fun1)
+t2: Thread = Thread(target=fun2)
+try:
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+except KeyboardInterrupt:
+    print('\nReceived keyboard interrupt, quitting threads.')
+    sys.exit(1)
